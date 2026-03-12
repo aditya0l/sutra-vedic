960976758e26edb855bf042a52104adf44583c94
Aditya Jaif <adityajaif@Adityas-MacBook-Air.local>
2026-03-12 11:41:33 +0530

    feat: Update Meta Pixel ID to 922885280329539

--- src/components/analytics/MetaPixel.tsx
+++ src/components/analytics/MetaPixel.tsx
@@ -18,7 +18,7 @@ export default function MetaPixel() {
                         t.src=v;s=b.getElementsByTagName(e)[0];
                         s.parentNode.insertBefore(t,s)}(window, document,'script',
                         'https://connect.facebook.net/en_US/fbevents.js');
-                        fbq('init', '1430449241887259');
+                        fbq('init', '922885280329539');
                         fbq('track', 'PageView');
                     `,
                 }}
@@ -28,7 +28,7 @@ export default function MetaPixel() {
                     height="1"
                     width="1"
                     style={{ display: 'none' }}
-                    src="https://www.facebook.com/tr?id=1430449241887259&ev=PageView&noscript=1"
+                    src="https://www.facebook.com/tr?id=922885280329539&ev=PageView&noscript=1"
                     alt=""
                 />
             </noscript>
