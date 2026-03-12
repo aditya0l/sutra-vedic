2e54bff02c0936afe018282bcc6b43edd6b22737
Aditya Jaif <adityajaif@Adityas-MacBook-Air.local>
2026-03-13 01:54:09 +0530

    fix: Move Meta Pixel and GTM to body for proper script loading

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -60,10 +60,10 @@ export default async function LocaleLayout({ children, params }: Props) {
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
                 <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700&family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet" />
-                <MetaPixel />
-                <GoogleTagManager />
             </head>
             <body className="min-h-screen flex flex-col">
+                <MetaPixel />
+                <GoogleTagManager />
                 <NextIntlClientProvider messages={messages}>
                     <CartProvider>
                         <WishlistProvider>
